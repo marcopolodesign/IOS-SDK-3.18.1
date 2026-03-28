e292ad2c8211e4eb3a432b3cfe1b436ea258aba0
Mat Aldao <m@marcopolo.agency>
2026-03-27 23:40:15 -0300

    chore: bump version to 1.0.13 (build 14) for TestFlight

--- SmartRingExpoApp/app.config.js
+++ SmartRingExpoApp/app.config.js
@@ -14,7 +14,7 @@ module.exports = {
     name,
     slug: 'smart-ring-expo-app',
     owner: 'mateoaldao',
-    version: '1.0.12',
+    version: '1.0.13',
     orientation: 'portrait',
     icon,
     userInterfaceStyle: 'dark',
@@ -27,7 +27,7 @@ module.exports = {
     ios: {
       supportsTablet: false,
       bundleIdentifier: bundleId,
-      buildNumber: '13',
+      buildNumber: '14',
       infoPlist: {
         NSBluetoothAlwaysUsageDescription: 'Focus needs Bluetooth to connect to your ring and sync health data',
         NSBluetoothPeripheralUsageDescription: 'Focus needs Bluetooth to connect to your ring and sync health data',
